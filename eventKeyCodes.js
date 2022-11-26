6ba215aced14fc24b82035c9ae09eaefe0439644
terrifricker <terrifricker@yahoo.com>
2022-11-25 16:09:18 -0800

    js: get event.key and event.code

--- eventKeyCodes.js
+++ eventKeyCodes.js
@@ -3,7 +3,8 @@ const input = document.querySelector("input");
 input.addEventListener("keydown", showKeyCode);
 
 function showKeyCode(event) {
-    const key = event.target
-    console.log(key);
-
+    const key = event.key;
+    const code = event.code;
+    console.log("key: ", key);
+    console.log("code: ", code);
 }
