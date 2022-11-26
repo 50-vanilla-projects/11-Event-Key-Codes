3d9b58683a9b399ae8c88400629159726adf3d98
terrifricker <terrifricker@yahoo.com>
2022-11-25 16:15:57 -0800

    js: delete createDisplay() function

--- eventKeyCodes.js
+++ eventKeyCodes.js
@@ -2,15 +2,6 @@ const input = document.querySelector("input");
 const display = document.querySelector(".display");
 
 input.addEventListener("keydown", showKeyCode);
-createDisplay();
-
-function createDisplay() {
-    let key = document.createElement("p");
-    key.classList.add("key");
-    let code = document.createElement("p");
-    code.classList.add("code");
-    display.append(key, code);
-}
 
 function showKeyCode(event) {
     const key = event.key;
