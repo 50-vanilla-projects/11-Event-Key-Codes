8a035452b8d8ca86af67a8ca6f6eed50bcd834a5
terrifricker <terrifricker@yahoo.com>
2022-11-25 20:54:34 -0800

    js: add count=0 to function, so first time is different

--- eventKeyCodes.js
+++ eventKeyCodes.js
@@ -2,10 +2,18 @@ const input = document.querySelector("input");
 const display = document.querySelector(".display");
 const keyDisplay = document.querySelector(".key");
 const codeDisplay = document.querySelector(".code");
+let count = 0;
 
 input.addEventListener("keydown", showKeyCode);
 
 function showKeyCode(event) {
+    if (count == 0) {
+        display.classList.remove("hidden");
+        count++;
+    } else {
+        keyDisplay.removeChild(keyDisplay.firstChild);
+        codeDisplay.removeChild(codeDisplay.firstChild);
+    }
     const key = document.createTextNode(event.key);
     const code = document.createTextNode(event.code);
     keyDisplay.appendChild(key);
