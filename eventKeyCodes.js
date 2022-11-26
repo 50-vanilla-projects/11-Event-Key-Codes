9abeb4ab878e2c6c99810b60b9eeecde3822f7a8
terrifricker <terrifricker@yahoo.com>
2022-11-25 20:50:31 -0800

    js: add key and code display areas and textNodes

--- eventKeyCodes.js
+++ eventKeyCodes.js
@@ -1,11 +1,13 @@
 const input = document.querySelector("input");
 const display = document.querySelector(".display");
+const keyDisplay = document.querySelector(".key");
+const codeDisplay = document.querySelector(".code");
 
 input.addEventListener("keydown", showKeyCode);
 
 function showKeyCode(event) {
-    const key = event.key;
-    const code = event.code;
-    console.log("key: ", key);
-    console.log("code: ", code);
+    const key = document.createTextNode(event.key);
+    const code = document.createTextNode(event.code);
+    keyDisplay.appendChild(key);
+    codeDisplay.appendChild(code);
 }
